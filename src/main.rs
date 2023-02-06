d7af9e8d2fc6c2c9959dad58a3cd7a45e753b407
Florentin Schäfer <florentin.schaefer@gmail.com>
2023-02-06 10:11:18 +0000

    Implement basic command handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenvy::dotenv;
+use serenity::async_trait;
+use serenity::model::application::command::Command;
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+use serenity::model::gateway::Ready;
+use serenity::model::prelude::command::CommandOptionType;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "bruh" => {
+                    if let Some(cdo) = command.data.options.get(0) {
+                        if let Some(sound) = &cdo.value {
+                            println!("Play {sound}");
+                        }
+                    } else {
+                        println!("Play bruh");
+                    }
+                    ":sunglasses:".to_string()
+                }
+                "ping" => "Hey, I'm alive! Temporarily, at least...".to_string(),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {why}");
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let _ = Command::set_global_application_commands(&ctx.http, |commands| {
+            commands
+                .create_application_command(|command| {
+                    command
+                        .name("bruh")
+                        .description("Play a sound")
+                        .create_option(|option| {
+                            option
+                                .name("sound")
+                                .description("Name of sound")
+                                .kind(CommandOptionType::String)
+                        })
+                })
+                .create_application_command(|command| {
+                    command.name("ping").description("A ping command")
+                })
+        })
+        .await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    // Build our client.
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
